f2db6e011569f12306cd42f91f92fee4c2290f52
JaziColumbres <143248516+JaziColumbres@users.noreply.github.com>
2024-03-04 20:29:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function transfer(){
+    alert("You will be redirected to the next page.");
+    window.open("second.html");    
+}
+
+function change(){
+    image.src='images/img2.jpg';
+}
+
+function revert(){
+    image.src='images/img1.jpg';
+}
